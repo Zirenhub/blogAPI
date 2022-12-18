012d0ad51a19ef3e158914bb019871c199adca89
Erdinch <erdinchorhan02@gmail.com>
2022-12-18 19:55:28 +0200

    basic design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,17 @@
 function App() {
-  return <div className="App" />;
+  return (
+    <header className="bg-primary flex items-center justify-between p-5">
+      <h1 className="text-3xl ">Personal Blog</h1>
+      <div>
+        <button
+          type="button"
+          className="transition-all hover:scale-105 hover:underline hover:underline-offset-4"
+        >
+          All Enteries
+        </button>
+      </div>
+    </header>
+  );
 }
 
 export default App;
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,7 +2,38 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F4EAD5',
+        secondary: '#DAE2B6',
+        third: '#CCD6A6',
+        background: '#FFFBE9',
+        dimWhite: 'rgba(255, 255, 255, 0.8)',
+      },
+      fontFamily: {
+        'Fira Sans': ['"Fira Sans"', 'sans-serif'],
+      },
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+        '6xl': '4.632rem',
+        '7xl': '5.232rem',
+        '8xl': '6.132rem',
+      },
+      screens: {
+        xs: '480px',
+        ss: '620px',
+        sm: '768px',
+        md: '1060px',
+        lg: '1200px',
+        xl: '1700px',
+      },
+    },
   },
   plugins: [],
 };
